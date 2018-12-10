a8f38dbfa990cea9de03d0ed303f00fae00cdd39
Vitalik <py4ina@gmail.com>
2018-12-10 20:50:49 +0200

    Prototype

--- src/main/java/questions/io_strim/FileInputStreamDemo.java
+++ src/main/java/questions/io_strim/FileInputStreamDemo.java
@@ -2,24 +2,35 @@ package questions.io_strim;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
 
 public class FileInputStreamDemo {
     public static void main(String[] args) {
-        int size;
 
-        try (FileInputStream fileInputStream = new FileInputStream("/home/vitalik/qwerty.txt")){
-            System.out.println("fileInputStream.available() : " + (size = fileInputStream.available()));
-            int n = size / 40;
-            System.out.println("n = " + n);
-            for (int i = 0; i < n; i++) {
-                System.out.println((char) fileInputStream.read());
-            }
-            System.out.println("fileInputStream.available() : " + (fileInputStream.available()));
+        try (FileInputStream fileInputStream = new FileInputStream("file0.txt");
+             FileInputStream fileInputStream2 = new FileInputStream("file1.txt")){
 
-            byte[] b = new byte[n];
-            System.out.println(new String(b, 0, n));
+            List<Character> strings0 = new LinkedList<>();
+            List<Character> strings1 = new LinkedList<>();
+            streamPrinter(fileInputStream, strings0);
+            streamPrinter(fileInputStream2, strings1);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void streamPrinter(FileInputStream fileInputStream, List<Character> listChar) throws IOException {
+        int i;
+        while ((i = fileInputStream.read()) != -1){
+            char symbol = (char) i;
+            System.out.print(symbol);
+            listChar.add(symbol);
+        }
+        System.out.println("--------------------------------------");
+        System.out.println(listChar);
     }
 }
