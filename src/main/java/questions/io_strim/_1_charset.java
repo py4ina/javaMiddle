e59134200f47a207f811bdcca095f65ec020b4aa
Vitalik <py4ina@gmail.com>
2018-12-13 17:13:55 +0200

    Adapter

--- src/main/java/questions/io_strim/_1_charset.java
+++ src/main/java/questions/io_strim/_1_charset.java
@@ -1,20 +1,23 @@
 package questions.io_strim;
 
-import java.io.UnsupportedEncodingException;
+import java.io.*;
+import java.net.URL;
 import java.nio.charset.Charset;
 
 public class _1_charset {
     public static void main(String[] args) throws UnsupportedEncodingException {
-        byte[] bytes = new byte[256];
 
-        for (int k = 0; k < 256; k++){
-            bytes[k] = (byte) (k - 128);
+        try(InputStream inputStream = new URL("https://jsehelper.blogspot.com/2016/01/java-core-1.html").openStream()) {
+            Reader reader = new InputStreamReader(inputStream, "UTF-8");
+
+            char[] buffer = new char[16];
+            int count;
+            while ((count = reader.read(buffer)) != -1){
+                System.out.print(new String(buffer, 0, count));
+            }
+            System.out.println(reader);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        System.out.println(new String(bytes, "UTF-8"));
-//
-//        for (String name : Charset.availableCharsets().keySet()){
-//            System.out.println(new String(bytes, name));
-//        }
     }
 }
