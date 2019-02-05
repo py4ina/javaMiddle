539a6d7974de09a239408dfa5dd7733b257c545e
py4ina <py4hina@gmail.com>
2019-02-05 21:55:28 +0200

    Java Core January: Collection. Лекция #19 (Часть 2) 9:59

--- src/main/java/questions/Main.java
+++ src/main/java/questions/Main.java
@@ -1,7 +1,56 @@
 package questions;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Main {
 
     public static void main(String[] args) {
+        Object ref = new User(25, "Mike");
+        Class aClass = ref.getClass();
+        Method[] methods = aClass.getMethods();
+        System.out.println(Arrays.toString(methods));
+    }
+}
+
+class User {
+    private int age;
+    private String name;
+
+    public User(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return age == user.age &&
+                Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(age, name);
     }
 }
