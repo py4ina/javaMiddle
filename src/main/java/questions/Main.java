f079848143ac2c248ec9bde1584b74b7e3d37fe4
py4ina <py4ina@gmail.com>
2019-02-06 22:06:13 +0200

    Java Core January: Collection. Лекция #20 (Часть 1) 0:00

--- src/main/java/questions/Main.java
+++ src/main/java/questions/Main.java
@@ -1,57 +1,59 @@
 package questions;
 
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Objects;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Object ref = new User(25, "Mike");
-        Class aClass = ref.getClass();
-        Method[] methods = aClass.getMethods();
-        System.out.println(Arrays.toString(methods));
+        NavigableSet<User> users = new TreeSet<>(User.BY_AGE);
 
+        users.add(new User(1, "A"));
+        users.add(new User(2, "B"));
+        users.add(new User(3, "C"));
+        users.add(new User(4, "D"));
+        users.add(new User(5, "I"));
+        users.add(new User(6, "F"));
+
+        System.out.println(users
+                .subSet(new User(2, "B"),
+                        true ,
+                        new User(5, "I"),
+                        true)
+        );
     }
 }
 
+@Data
+@ToString
 class User {
+
+    public static final Comparator<User> BY_AGE = new AgeComparator();
+    public static final Comparator<User> BY_NAME = new NameComparator();
+
     private int age;
     private String name;
 
-    public User(int age, String name) {
+    User(int age, String name) {
         this.age = age;
         this.name = name;
     }
+}
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+class AgeComparator implements Comparator<User> {
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof User)) return false;
-        User user = (User) o;
-        return age == user.age &&
-                Objects.equals(name, user.name);
+    public int compare(User user1, User user2) {
+        return user1.getAge() - user2.getAge();
     }
+}
+
+class NameComparator implements Comparator<User> {
 
     @Override
-    public int hashCode() {
-
-        return Objects.hash(age, name);
+    public int compare(User user1, User user2) {
+        return user1.getName().compareTo(user2.getName());
     }
 }
