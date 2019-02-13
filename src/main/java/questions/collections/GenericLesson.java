2f61445e8a817fa19eb4c5a6e3e8973499f7f73c
Vitalik <py4ina@gmail.com>
2019-02-13 11:26:24 +0200

    Generic

--- src/main/java/questions/collections/GenericLesson.java
+++ src/main/java/questions/collections/GenericLesson.java
@@ -3,14 +3,33 @@ package questions.collections;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.min;
+
 public class GenericLesson {
     public static void main(String[] args) {
-//        Printer<Integer> printerInteger = new Printer<>();
-//        printerInteger.setData(new A(123).getData());
-//        printerInteger.printData();
+        Integer i = mock(1);
+    }
 
-        Printer<String, Integer> printer = new Printer<>("AA", 22);
-        printer.printData();
+    private static void testGenericsA() {
+        ArrayList<Integer> integers = new ArrayList<>(10);
+        integers.set(0, new Integer(1));
+        ArrayList<Object> objects = new ArrayList<>(10);
+        copyGen(integers, objects);
+        Object noCast = objects.get(0);
+    }
+
+    public static <T> void copyGen(ArrayList<? extends T> src, ArrayList<? super T> dst ) {
+        int length = min(src.size(), dst.size());
+        for (int k = 0; k < length; k++) {
+            dst.set(k, src.get(k));
+        }
+    }
+
+    public static <T> T mock(T arg) {
+        return null;
     }
 }
 
