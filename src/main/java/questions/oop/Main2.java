b4a9a54c3a9d9af099b7c8de446cf26a413251fc
Ukrpol <yurii.horobey@redtag.com.ua>
2019-02-22 14:01:48 +0200

    Java Core: OOP1. Лекция #21 (часть2) 34:42

---
+++ src/main/java/questions/oop/Main2.java
@@ -0,0 +1,18 @@
+package questions.oop;
+
+public class Main2 {
+    public static void main(String[] args) {
+
+
+        System.out.println();
+    }
+}
+
+class A{
+    public int iCopy = getI();
+    private int i = 1;
+
+    private int getI() {
+        return i;
+    }
+}
