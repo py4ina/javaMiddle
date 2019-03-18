45930b0fac77171fa094ce0c1d1828a5454e6575
Vitalik <py4ina@gmail.com>
2019-03-18 19:52:24 +0200

    Java Core: Исключения. Лекция #5 (Часть 6) 03:28 https://www.youtube.com/watch?v=6QxytA0ryvs&index=6&list=PLoij6udfBncilt7XcB6XXUovRcqxsNY-j

--- src/main/java/questions/exception/ExceptionTest_TC_0.java
+++ src/main/java/questions/exception/ExceptionTest_TC_0.java
@@ -4,11 +4,14 @@ public class ExceptionTest_TC_0 {
     public static void main(String[] args) {
         try {
             System.out.println(0);
-            throw new RuntimeException();
+            throw new NullPointerException();
         } catch (NullPointerException e) {
             System.out.println(1);
+            throw e;
+        } catch (ArithmeticException e){
+            System.out.println(2);
         }
 
-        System.out.println(2);
+        System.out.println(3);
     }
 }
