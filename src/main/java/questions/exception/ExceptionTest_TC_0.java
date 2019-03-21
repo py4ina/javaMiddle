bb3f01034d63f3f45f4aa64a6774641671099c6f
Vitalik <py4ina@gmail.com>
2019-03-21 10:31:19 +0200

    Java Core: Исключения. Лекция #6 (Часть 2) 03:28 https://www.youtube.com/watch?v=CxnjeluLlkE&list=PLoij6udfBncgll4Wgd_DukdD9L6K-WT0s&index=2

--- src/main/java/questions/exception/ExceptionTest_TC_0.java
+++ src/main/java/questions/exception/ExceptionTest_TC_0.java
@@ -1,18 +1,8 @@
 package questions.exception;
 
 public class ExceptionTest_TC_0 {
-    public static void main(String[] args) {
-        System.out.println(f());
+    public static void main(String[] args) throws Exception{
+        throw new Exception();
 
-    }
-
-    private static int f() {
-        try {
-            System.out.println(0);
-            return 42;
-        } finally {
-            System.out.println(1);
-            return 24;
-        }
     }
 }
