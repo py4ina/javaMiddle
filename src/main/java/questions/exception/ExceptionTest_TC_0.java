b8754457e80137d629620b2dd66a022cdaac02a5
Vitalik <py4ina@gmail.com>
2019-03-22 13:55:04 +0200

    Java Core: Исключения. Лекция #6 (Часть 2) 7:50 https://www.youtube.com/watch?v=aFk9cukSs30&list=PLoij6udfBncgll4Wgd_DukdD9L6K-WT0s&index=7

--- src/main/java/questions/exception/ExceptionTest_TC_0.java
+++ src/main/java/questions/exception/ExceptionTest_TC_0.java
@@ -1,14 +1,11 @@
 package questions.exception;
 
-import java.io.IOException;
-
 public class ExceptionTest_TC_0 {
-    public static void main(String[] args){
-        try {
-            throw new IOException();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) {
+       f();
+    }
 
+    private static void f() {
+        f();
     }
 }
