aca76d8a122ccf500b8e82406f6b2d2b34b9fd17
py4ina <py4ina@gmail.com>
2019-03-30 19:20:54 +0200

    Шаблоны Java. Стратегия (Strategy)

---
+++ src/main/java/questions/_enum/Day.java
@@ -0,0 +1,24 @@
+package questions._enum;
+
+import java.util.List;
+
+enum Day{
+
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY;
+
+
+}
+
+class Program{
+    public static void main(String[] args) {
+
+
+        System.out.println();
+    }
+}
