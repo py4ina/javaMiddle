14a53875c7e5526031f57e888619010cd7f4e933
py4ina <py4ina@gmail.com>
2019-04-06 20:33:31 +0300

    Junior.February2014.Servlets#1 (Часть 2)
    https://www.youtube.com/watch?v=Ix_0ZyK-AT4&list=PLoij6udfBncioun9-sBwpkpTit1SIhWko&index=27

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,7 @@
 public class Main {
-    public static void main(String[] args) throws IllegalArgumentException, IllegalArgumentException {
-        sqr(2);
-    }
-
-    public static double sqr(double arg) {
-
-        throw new RuntimeException();
-    }
-
-}
-
- class Formatter {
-    public String format(String value) {
-        return "["+value+"]";
-    }
-}
-
-class TestNPE {
-    public static String handle(Formatter f, String s) {
-        if("".equals(s)) {
-            return "(none)";
-        }
-        return f.format(s.trim());
+    public static void main(String[] args){
+        System.out.println("Example 2 - Start");
+        System.exit(0);
+        System.out.println("Example 2 - End");
     }
 }
