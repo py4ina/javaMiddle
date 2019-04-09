68bdce5ab4506f0d3854f0b3d667b84a6873253e
Vitalik <py4ina@gmail.com>
2019-04-09 12:50:02 +0300

    Junior.February2014.Servlets#1 (Часть 5) 34:39 https://www.youtube.com/watch?v=DZxRcBUihNU&list=PLoij6udfBncioun9-sBwpkpTit1SIhWko&index=30

---
+++ src/main/java/com/learn/servlet/filter/RequestInfoFilter.java
@@ -0,0 +1,18 @@
+package com.learn.servlet.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class RequestInfoFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        chain.doFilter(req, resp);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
