18d11747f2e2c679735368d3e0201cb51abcb527
Vitalik <py4ina@gmail.com>
2019-04-12 14:57:07 +0300

    Junior.February2014.Servlets#4 (Часть 4) 00:05
    https://www.youtube.com/watch?v=aN4aWgmybuA&list=PLoij6udfBncioun9-sBwpkpTit1SIhWko&index=35

--- src/main/java/com/learn/Main.java
+++ src/main/java/com/learn/Main.java
@@ -1,14 +1,20 @@
 package com.learn;
 
 public class Main {
-    static Integer str = new Integer(1);
 
-    public static void changeIt(Integer s) {
-        s = 2;
+    static String str = "Value 1";
+
+    public static void changeIt(String s) {
+        s = "Value 2";
     }
 
     public static void main(String[] args) {
-        changeIt(str);
-        System.out.println(str);
+//        changeIt(str);
+//        System.out.println(str);
+        String s1 = new String("A");
+        String s2 = s1;
+        System.out.println(s1 + ", "+ s2);
+        s1 = new String("B");
+        System.out.println(s1 + ", "+ s2);
     }
 }
