d2379ca8d1093a4fde57452e555af25e4cda8bd0
py4ina <py4ina@gmail.com>
2019-04-20 15:17:29 +0300

    Junior.February2014.Servlets#6 (Часть 1) 00:00
    https://www.youtube.com/watch?v=xHKT8BUZt54&list=PLoij6udfBncioun9-sBwpkpTit1SIhWko&index=38

--- src/main/java/com/learn/servlet/controller/CookieMockController.java
+++ src/main/java/com/learn/servlet/controller/CookieMockController.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "CookieMockController")
 public class CookieMockController extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
