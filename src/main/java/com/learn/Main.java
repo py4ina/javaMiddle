faaf7a15451f362da793114790f1134033014dd7
py4ina <py4ina@gmail.com>
2019-05-01 19:40:32 +0300

    http://www.quizful.net/interview/java/T00PbdMFLm9l

--- src/main/java/com/learn/Main.java
+++ src/main/java/com/learn/Main.java
@@ -3,49 +3,10 @@ package com.learn;
 public class Main {
 
     public static void main(String[] args) {
-        if (a() && b()){
+        int i, n = 10;
 
+        for (i = 0; i < n; n--) {
+            System.err.println("*");
         }
-    }
-
-    private static boolean a (){
-        System.out.println("a");
-        return true;
-    }
-
-    private static boolean b (){
-        System.out.println("b");
-        return true;
-    }
-}
-
-class Superclass {
-    public static int field;
-    public static int getField() { return field; }
-    public static void setField(int value) { field = value; }
-}
-
-class Superclass2 {
-    public int field;
-    public int getField() { return field; }
-    public void setField(int value) { field = value; }
-}
-
-class Subclass extends Superclass2 {
-    public int field;
-
-    public int getField() {
-        return field;
-    }
-
-    public void setField(int value) {
-        field = value;
-    }
-
-    public static void main(String[] params) {
-        Superclass2 sup = new Subclass();
-//        System.out.println(this.field);
-        sup.field = 1;
-        System.out.println("Subclass field: " + sup.getField());
     }
 }
