662adf97cce070292010394f98a97786a5512fcf
Vitalik <py4ina@gmail.com>
2019-06-01 20:00:27 +0300

    http://java-online.ru/junit-mockito.xhtml

---
+++ src/main/java/com/learn/testing/Mockito/ICalculator.java
@@ -0,0 +1,8 @@
+package com.learn.testing.Mockito;
+
+public interface ICalculator {
+    public double add      (double d1, double d2);
+    public double subtract (double d1, double d2);
+    public double multiply (double d1, double d2);
+    public double divide   (double d1, double d2);
+}
