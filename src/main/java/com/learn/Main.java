f6049fff45e3669e89a995a6e503fc82fc7b12c4
Vitalik <py4ina@gmail.com>
2019-10-10 18:27:55 +0300

    http://dmivic.chat.ru/JDBC/connection.doc.html

--- src/main/java/com/learn/Main.java
+++ src/main/java/com/learn/Main.java
@@ -1,14 +1,28 @@
 package com.learn;
 
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-    private static int i = 0;
+    private final static String URL = "jdbc:mysql://10.4.4.8:3306/ukrpol_print_label";
+    private final static String USER_NAME = "root";
+    private final static String PASSWORD = "UkrPol_123";
+
     public static void main(String[] args) {
-        System.out.println(i++);
+
         try {
-            main(args);
-        } catch (Throwable e){
-            System.out.println(e);
-           main(args);
+            Driver driver = new FabricMySQLDriver();
+            DriverManager.registerDriver(driver);
+            Connection connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+            if (!connection.isClosed()){
+                System.out.println("+");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
