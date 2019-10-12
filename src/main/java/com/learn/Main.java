f23450e0857bf32e48db8a616c817f3af7c4ddfa
py4ina <py4ina@gmail.com>
2019-10-12 17:11:27 +0300

    CallableStatement
    http://dmivic.chat.ru/JDBC/callablestatement.doc.html

--- src/main/java/com/learn/Main.java
+++ src/main/java/com/learn/Main.java
@@ -1,28 +1,29 @@
 package com.learn;
 
-import com.mysql.fabric.jdbc.FabricMySQLDriver;
-
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
-    private final static String URL = "jdbc:mysql://10.4.4.8:3306/ukrpol_print_label";
+    private final static String URL = "jdbc:mysql://10.4.4.8:3306/ukr_pol";
     private final static String USER_NAME = "root";
     private final static String PASSWORD = "UkrPol_123";
 
+    private static final String QUERY = "SELECT * FROM user";
+
     public static void main(String[] args) {
+        System.out.println("Start");
 
-        try (Connection connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD)) {
-//            Driver driver = new FabricMySQLDriver();
-//            DriverManager.registerDriver(driver);
+        try (Connection connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+             CallableStatement statement = connection.prepareCall("{call getAllWorkingTerminals}");
+             ResultSet resultSet = statement.executeQuery()){
 
-            if (!connection.isClosed()){
-                System.out.println("+");
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("name"));
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            System.out.println("Stop");
         }
     }
 }
