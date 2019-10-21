62c5bfeee197584482c53ea935d040aa2de3a768
py4ina <py4ina@gmail.com>
2019-10-21 21:09:29 +0300

    https://www.youtube.com/watch?v=xphIRTGshco
    https://jsehelper.blogspot.com/2016/01/java-collections-framework-2.html

--- src/main/java/com/learn/Main.java
+++ src/main/java/com/learn/Main.java
@@ -1,51 +1,24 @@
 package com.learn;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        Queue<Integer> queue = new PriorityQueue<>();
-        Random random = new Random();
-        for (int i = 0; i < 7; i++) {
-            queue.add(new Integer(random.nextInt(100)));
-        }
-
-        for (int i = 0; i < 7; i++) {
-            Integer integer = queue.poll();
-            System.out.println("Обрабатываем Integer:" + integer);
-        }
-
-        Queue<Customer> customers = new PriorityQueue<>(7, Comparator.comparingInt(Customer::getId));
-        addDataToQueue(customers);
-        pollDataFromQueue(customers);
-
-
+        Set<Integer> current = new TreeSet<>();
+        current.add(1);
+        current.add(2);
+        current.add(3);
+        Set<Integer> previously = new TreeSet<>();
+        previously.add(3);
+        previously.add(2);
+        previously.add(6);
+//        Collections.
+        System.out.println(current);
+        System.out.println(current.retainAll(previously));
+        System.out.println(current);
     }
-
-    private static void addDataToQueue(Queue<Customer> customerPriorityQueue){
-        Random random = new Random();
-        for (int i = 0; i < 7; i++) {
-            int id = random.nextInt(100);
-            customerPriorityQueue.add(new Customer(id, "Vova_"+id));
-        }
-    }
-
-    private static void pollDataFromQueue(Queue<Customer> customerPriorityQueue){
-        while (true){
-            Customer customer = customerPriorityQueue.poll();
-            if (customer == null) break;
-            System.out.println("Обработка клиента с id=" + customer.getId());
-        }
-    }
-}
-
-@Data
-@AllArgsConstructor
-class Customer {
-    private int id;
-    private String name;
 }
