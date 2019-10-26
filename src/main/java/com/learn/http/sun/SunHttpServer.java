57638f47d6a0b3367007da87ebb28f8246e4db66
py4ina <py4ina@gmail.com>
2019-10-26 21:15:04 +0300

    https://www.youtube.com/watch?v=cd5WB26HIzw&list=PLwcDaxeEINaeOqd15C3D0PwotuKknFwxF&index=151
    Java Junior February: HTTP. Лекция #9 (Часть 4)
    
    19:06

---
+++ src/main/java/com/learn/http/sun/SunHttpServer.java
@@ -0,0 +1,13 @@
+package com.learn.http.sun;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.net.InetSocketAddress;
+
+import static com.sun.net.httpserver.spi.HttpServerProvider.provider;
+
+public class SunHttpServer {
+    public static void main(String[] args) {
+        HttpServer server = provider().createHttpServer(new InetSocketAddress());
+    }
+}
