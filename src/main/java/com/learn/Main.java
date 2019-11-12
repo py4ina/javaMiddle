7bb0613c21370a919070a14edf4670d594098c7d
Vitalik <py4ina@gmail.com>
2019-11-12 17:42:35 +0200

    44 st

--- src/main/java/com/learn/Main.java
+++ src/main/java/com/learn/Main.java
@@ -2,22 +2,23 @@ package com.learn;
 
 
 import java.util.List;
+import java.util.function.BinaryOperator;
 
 import static java.util.Arrays.asList;
 
 public class Main {
     public static void main(String[] args) {
+        BinaryOperator<Integer> accumulator = (acc, element) -> acc + element;
+
         List<Integer> list = asList(1, 2, 3, 4);
 
-        int i = list.stream()
-                .reduce(0, (x, y) ->
-                        {
-                            System.out.println(x + ", " + y);
-                            return x + y;
-                        }
-                );
-        System.out.println(i);
+        int sum = accumulator.apply(
+                accumulator.apply(
+                        accumulator.apply(0, 1),
+                        2),
+                3);
 
+        System.out.println(sum);
 
     }
 }
