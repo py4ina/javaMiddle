98111eac3b69866d248a7b3a6b16f586d21bc5a4
Vitalik <py4ina@gmail.com>
2020-01-06 11:06:55 +0200

    https://www.dokwork.ru/2012/02/http-java-http.html

--- src/main/java/com/learn/Main.java
+++ src/main/java/com/learn/Main.java
@@ -1,21 +1,11 @@
 package com.learn;
 
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException, ScriptException {
-        ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
-        ScriptEngine engine = scriptEngineManager.getEngineByName("nashorn");
-
-        engine.eval("print('Hello World!!!')");
-
-        FileReader fileReader = new FileReader("/home/vitalik/IdeaProjects/javaMiddle/src/main/java/com/learn/demo.js");
-        engine.eval(fileReader);
+    public static void main(String[] args) throws IOException, ScriptException {
 
     }
 }
