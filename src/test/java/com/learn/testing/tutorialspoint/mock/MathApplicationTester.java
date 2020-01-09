39af0b3c045154a8111f376e7f37d30a6addd9c6
Vitalik <py4ina@gmail.com>
2020-01-09 12:48:07 +0200

    https://www.tutorialspoint.com/mockito/mockito_varying_calls.htm

--- src/test/java/com/learn/testing/tutorialspoint/mock/MathApplicationTester.java
+++ src/test/java/com/learn/testing/tutorialspoint/mock/MathApplicationTester.java
@@ -1,13 +1,13 @@
 package com.learn.testing.tutorialspoint.mock;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MathApplicationTester {
@@ -23,7 +23,23 @@ public class MathApplicationTester {
         //add the behavior of calc service to add two numbers
         when(calcService.add(10.0,20.0)).thenReturn(30.00);
 
+        //add the behavior of calc service to subtract two numbers
+        when(calcService.subtract(20.0,10.0)).thenReturn(10.00);
+
         //test the add functionality
-        Assert.assertEquals(mathApplication.add(10.0, 20.0),30.0,0);
+        assertEquals(calcService.add(10.0, 20.0),30.0,0);
+        assertEquals(calcService.add(10.0, 20.0),30.0,0);
+        assertEquals(calcService.add(10.0, 20.0),30.0,0);
+
+        assertEquals(mathApplication.subtract(20.0, 10.0),10.0,0.0);
+
+        //default call count is 1
+        verify(calcService).subtract(20.0, 10.0);
+
+        //check if add function is called three times
+        verify(calcService, times(3)).add(10.0, 20.0);
+
+        //verify that method was never called on a mock
+        verify(calcService, never()).multiply(10.0,20.0);
     }
 }
