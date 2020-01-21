37ced902bc7bdb533591d1e045697937279a43b2
Vitalik <py4ina@gmail.com>
2020-01-21 17:59:08 +0200

    https://www.tutorialspoint.com/mockito/mockito_spying.htm

--- src/main/java/com/learn/Main.java
+++ src/main/java/com/learn/Main.java
@@ -1,21 +1,45 @@
 package com.learn;
 
 
-import java.time.*;
-import java.util.Date;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-    public static void main(String[] args) {
-//        LocalDateTime parse = LocalDateTime.parse("2020-01-14 14:35:12.0");
-        LocalDate now = LocalDate.now();
-        LocalTime lt = LocalTime.parse("02:53:40");
-        Instant instant = lt.atDate(LocalDate.of(now.getYear(), now.getMonth(), now.getDayOfMonth())).
-                atZone(ZoneId.systemDefault()).toInstant();
-        Date date = Date.from(instant);
-        System.out.println(date);
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        Map<String, String> map = main.readFile();
+        System.out.println(map);
+    }
 
-        LocalDateTime localDateTime = LocalDateTime.ofInstant(date.toInstant(),
-                ZoneId.systemDefault());
-        System.out.println(localDateTime);
+    private final static String PATH = "/etc/hostname";
+    private Path path = Paths.get(PATH);
+
+    public  Map<String, String> readFile() {
+        Map<String, String> source = null;
+        try {
+            source = fileParser(Files.readAllLines(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return source;
+    }
+
+    private Map<String, String> fileParser(List<String> strings){
+        if (strings.size() > 1){
+            throw new UnsupportedOperationException();
+        }
+        Map<String, String> map = new HashMap<>();
+        for (String line : strings){
+            String value = line.replaceAll("[^0-9?!\\.]","");
+            map.put("terminal_id", value);
+        }
+        return map;
     }
 }
